99fbbf26ec2445dcdfa7d563ef23b510bde0a680
Breno AllenCS <brenoallencs@gmail.com>
2022-07-03 00:52:54 -0300

    Login page

--- lib/screens/login /login.dart
+++ lib/screens/login /login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 class Login extends StatefulWidget{
   @override
@@ -8,69 +9,175 @@ class Login extends StatefulWidget{
 }
 
 class _LoginState extends State<Login>{
+  bool _passwordVisibility = false;
+  TextEditingController _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context){
     return Scaffold(
       backgroundColor: Color(0xFFEFF2F2),
       body: SafeArea(
-        child: Column(
-          children: [
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height: 350,
-              child: Center(child: Image.asset('lib/assets/gummy-camping.png')),
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              child: const Padding(
-                padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
-                child: Text(
-                  'Login',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 32
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: Column(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 350,
+                child: Center(child: Image.asset('lib/assets/gummy-camping.png')),
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: const Padding(
+                  padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 32
+                    ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              child: Form(
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(30, 30, 30, 0),
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                          hintText: 'E-mail',
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: Form(
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(30, 30, 30, 0),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            hintText: 'E-mail',
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Theme.of(context).primaryColor,
+                              )
+                            ),
+                          ),
+                          validator: (String? value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter some text';
+                            }
+                            return null;
+                          },
                         ),
-                        validator: (String? value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter some text';
-                          }
-                          return null;
-                        },
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(30.0),
+                        child: TextFormField(
+                          controller: _passwordController,
+                          decoration: InputDecoration(
+                            hintText: 'Senha',
+                            suffixIcon: InkWell(
+                              onTap: () => setState(
+                                () => _passwordVisibility = !_passwordVisibility,
+                              ),
+                              child: Icon(
+                                _passwordVisibility
+                                    ? Icons.visibility_off_outlined
+                                    : Icons.visibility_outlined,
+                                color: Theme.of(context).primaryColor,
+                                size: 22,
+                              ),
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Theme.of(context).primaryColor,
+                              )
+                            )
+                          ),
+                          obscureText: !_passwordVisibility,
+                          validator: (String? value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your password';
+                            }
+                            return null;
+                          },
+                        ),
+                      )
+                    ],
+                  )
+                ),
+              ),
+              Container(
+                alignment: Alignment.centerRight,
+                width: MediaQuery.of(context).size.width,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 30, 0),
+                  child: GestureDetector(
+                    onTap: () {
+                      const snackBar = SnackBar(content: Text('Desenvolvendo ainda'));
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    },
+                    child: Text(
+                      'Esqueceu a senha?',
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontSize: 16,
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(30.0),
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                          hintText: 'Senha',
-                        ),
-                        validator: (String? value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter your password';
-                          }
-                          return null;
-                        },
-                      ),
-                    )
-                  ],
-                )
+                  ),
+                ),
               ),
-            )
-          ],
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: SizedBox(
+                  width: 380,
+                  height: 45,
+                  child: ElevatedButton(
+                    onPressed: (() {
+                      
+                    }),
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: Theme.of(context).primaryColor,
+                    )
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 400,
+                child: Divider(
+                  color: Theme.of(context).primaryColor,
+                  thickness: 1,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: SizedBox(
+                  width: 250,
+                  height: 45,
+                  child: SignInButton(
+                    Buttons.Google,
+                    text: "Sign up with Google",
+                    onPressed: () {},
+                  )
+                ),
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 16, 0, 0),
+                  child: Center(
+                    child: Text(
+                      'Registre-se',
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                  ),
+                )
+              )
+            ],
+          ),
         ),
       ),
     );
